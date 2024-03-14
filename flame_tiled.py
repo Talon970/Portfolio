8e6e36367cd7f2166da00e6b756973f9030015e4
Talon970 <david.tretow@web.de>
2024-03-14 12:04:00 +0100

    12-Week-Year

---
+++ flame_tiled.py
@@ -0,0 +1,5 @@
+try:
+    import flame_tiled
+    print("flame-tiled wurde erfolgreich installiert und importiert.")
+except ImportError:
+    print("Es gab ein Problem beim Importieren von flame-tiled. Möglicherweise wurde es nicht korrekt installiert.")
