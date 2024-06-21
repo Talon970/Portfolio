a70f9edbb4ec4e51f1a6d6a866ca9172271fb060
Talon970 <99196480+Talon970@users.noreply.github.com>
2024-06-21 21:34:32 +0300

    Delete 0022_constatnts.dart

--- 0022_constatnts.dart
+++
@@ -1,6 +0,0 @@
-const double kDouble5 = 5.0;
-const double kDouble10 = 10.0;
-const double kDouble20 = 20.0;
-const double kDouble40 = 40.0;
-
-const String baconDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi iaculis neque sem, ut tincidunt mi feugiat sed. Sed ac placerat nulla. Ut ut malesuada mauris. Quisque commodo eu orci nec elementum. Ut felis diam, semper nec nisi et, congue egestas dolor. Nulla facilisi. Donec varius fermentum pretium. Donec ultrices dignissim malesuada. Quisque mattis nunc in justo eleifend, in hendrerit ex dictum. Quisque a arcu arcu. Maecenas non posuere velit, sit amet consequat risus. Sed at metus odio. Nullam malesuada vel tellus non dictum. Cras in magna tortor. Duis molestie lectus a malesuada venenatis. ";
