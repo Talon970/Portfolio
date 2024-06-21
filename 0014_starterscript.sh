c5fdc97476b71b0abc6bd61503404fef2a2ef497
Talon970 <99196480+Talon970@users.noreply.github.com>
2024-06-21 21:31:58 +0300

    Delete 0014_starterscript.sh

--- 0014_starterscript.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# Warten auf Internetverbindung
-while ! ping -c 1 -W 1 8.8.8.8; do
-    sleep 1
-done
-
-firefox https://www.hackthebox.com/ https://chat.openai.com/c/9a2b1371-1a22-4a7f-849b-8caa39ffb855 https://pomofocus.io/ https://www.youtube.com/watch?v=eWLDAAMsD-c https://github.com/Talon970/python-html-css-javascript_basics https://leetcode.com/
