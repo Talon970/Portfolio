de1d54b31df9b4c076c42360a039b5032ad55f4b
Talon970 <99196480+Talon970@users.noreply.github.com>
2024-06-21 21:37:01 +0300

    Delete readcsv_from_url.py

--- readcsv_from_url.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-list = pd.read_csv("https://www.football-data.co.uk/mmz4281/2324/E0.csv")
-
-print(list)
-# https://www.football-data.co.uk/data.php
-
-list.rename(columns={"FTHG":"home_goals",
-                    "FTAG":"away_goals"}, inplace=True)
