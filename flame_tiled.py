ba830451ab02e9c656d12092fa72a56bf237aa85
Talon970 <99196480+Talon970@users.noreply.github.com>
2024-06-21 21:36:27 +0300

    Delete flame_tiled.py

--- flame_tiled.py
+++
@@ -1,5 +0,0 @@
-try:
-    import flame_tiled
-    print("flame-tiled wurde erfolgreich installiert und importiert.")
-except ImportError:
-    print("Es gab ein Problem beim Importieren von flame-tiled. Möglicherweise wurde es nicht korrekt installiert.")
