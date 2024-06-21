cbf031c1e60b9bc86681a270fb582643af962f2b
Talon970 <99196480+Talon970@users.noreply.github.com>
2024-06-21 21:32:28 +0300

    Delete 0014_listmanipulation.py

--- 0014_listmanipulation.py
+++
@@ -1,8 +0,0 @@
-#Aufgabe: Schreibe ein Programm, das eine Liste von Zahlen sortiert und Duplikate entfernt.
-def sort_and_remove_duplicates(numbers):
-    # Sortiert die Liste und entfernt Duplikate
-    return sorted(set(numbers))
-
-# Testen der Funktion
-numbers = [3, 6, 2, 3, 2, 6, 8]
-result = sort_and_remove_duplicates(numbers)
